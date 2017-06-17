3802318eb0e4758e895ab938b4cbb398e4889947
Jose Velez <jose.velez363@gmail.com>
2017-06-17 11:53:33 -0400

    Still broken trying to display one component

--- app/app.js
+++ app/app.js
@@ -2,8 +2,15 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-// Grabs the Routes
-var routes = require("./config/routes");
+// // Grabs the Routes
+// var routes = require("./config/routes");
+//
+// var apiKey = "a71a6360524e4c958030b87027a0fcd9";
+//
+// var queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=";
+
+var Main = require("./components/main");
+
 
 // Renders the contents according to the route page.
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"));
---
+++ app/components/main.js
@@ -0,0 +1,51 @@
+// Include React
+var React = require("react");
+
+// Include the subcomponents
+var Search = require("./search");
+
+// Creating the main component
+
+var Main = React.createClass({
+
+
+  // Render the page
+
+  render: function(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="jumbotron">
+            <h2>NYT React</h2>
+            <p>
+              <em>NYT React</em>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">Search</h3>
+              </div>
+              <div className="panel-body text-center">
+                <form>
+                  <label for="topic">Topic:</label>
+                  <input type="text" name="topic" id="topic-name">
+                  <label for="startYear">Start Year:</label>
+                  <input type="text" name="startYear" id="start-year">
+                  <label for="endYear">End Year:</label>
+                  <input type="text" name="endYear" id="end-year">
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+          <result/>
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
